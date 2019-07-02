d4d79b5b49d4e50cd09c6aaeade6e268c0697f1a
Peter Zhang <peter.zhang@simpro.co>
2019-07-02 17:34:31 +1000

    #init push

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # bitbucket-server-cli-tools
 a place for bitbucket server cli tools
+
+##cpr - create pull request
+
+
+ 
+ ###Example: 
+ 
+  * Set global environment variable pointing to the cpr config file path (Example in Linux ~/.bashrc)
+     ~~~
+     export CPR_CONFIG_FILE_PATH="$HOME/cpr_config.yaml"
+     ~~~
+    then directly execute it , cpr will check $CPR_CONFIG_FILE_PATH
+    ~~~
+      $ cpr
+    ~~~
+  * Or, Reading setting from load flag and execute it
+      ~~~
+        $ cpr --load '/home/example_cpr_config.yaml'
+      ~~~
+  * Or, setup|modify flags values
+    ~~~
+    $ cpr --username fristname.lastname --password '###' --url 'https://bitbucket.example.com' --destBranch 'release/1.2.9' --title 'This is a sample title' --description 'Please check on Line:100' --debug --reviewer firstname.lastname --reviewer firstname.lastname
+    ~~~
+    
+  ###Precedence
+  The precedence for flag value sources is as follows (highest to lowest):
+  0. Command line flag value from user
+  1. Environment variable (if specified)
+  2. Configuration file (if specified)
+  3. Default defined on the flag
+  
+  
+###Flags
+   ~~~
+      --load value                    load .yaml config file from the path or from environment variable [$CPR_CONFIG_FILE_PATH]
+      --username value                Bitbucket account username
+      --password value                Bitbucket account password
+      --url value                     bitbucket server url (default: "https://bitbucket.simprocloud.com")
+      --destBranch value              PR destination branch
+      --title value                   PR title, branch name will be used if the title is not given
+      --description value             PR description, could be empty
+      --debug                         turn debug on, will turn on all arguments and flags value
+      --reviewers firstName.lastName  PR reviewers firstName.lastName
+      --help, -h                      show help
+      --version, -v                   print the version
+~~~
---
+++ cpr.go
@@ -0,0 +1,222 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"gopkg.in/urfave/cli.v1"
+	"gopkg.in/urfave/cli.v1/altsrc"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
+)
+
+func main() {
+
+	var username, password, serverUrl, destination, inputTitle, description string
+	app := cli.NewApp()
+	app.Name = "CPR"
+	app.Usage = "A tool creating pull request in bitbucket"
+	app.EnableBashCompletion = true
+	app.Author = "Zehui Zhang"
+	app.Email = "peter.zhang@simpro.co"
+	app.Version = "1.0"
+	app.Compiled = time.Now()
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:   "load",
+			EnvVar: "CPR_CONFIG_FILE_PATH",
+			Usage:  "load .yaml config file from the path or load from environment variable in default",
+		},
+		altsrc.NewStringFlag(
+			cli.StringFlag{
+				Name:        "username",
+				Usage:       "Bitbucket account username",
+				Destination: &username,
+			}),
+		altsrc.NewStringFlag(
+			cli.StringFlag{
+				Name:        "password",
+				Usage:       "Bitbucket account password",
+				Destination: &password,
+			}),
+		altsrc.NewStringFlag(
+			cli.StringFlag{
+				Name:        "url",
+				Usage:       "bitbucket server url ",
+				Value:       "https://bitbucket.simprocloud.com",
+				Destination: &serverUrl,
+			}),
+		altsrc.NewStringFlag(
+			cli.StringFlag{
+				Name:        "destBranch",
+				Usage:       "PR destination branch",
+				Destination: &destination,
+			}),
+		altsrc.NewStringFlag(
+			cli.StringFlag{
+				Name:        "title",
+				Usage:       "PR title, branch name will be used if the title is not given",
+				Destination: &inputTitle,
+			}),
+		cli.StringFlag{
+			Name:        "description",
+			Usage:       "PR description, could be empty",
+			Destination: &description,
+		},
+		cli.BoolFlag{
+			Name:  "debug",
+			Usage: "turn debug on, will turn on all arguments and flags value",
+		},
+		altsrc.NewStringSliceFlag(
+			cli.StringSliceFlag{
+				Name:  "reviewer",
+				Usage: "PR reviewer `firstName.lastName`, could be multiple",
+			}),
+	}
+	app.Action = func(c *cli.Context) error {
+
+		var currentBranch string
+		var PRTitlePointer *string
+
+		if password == "" {
+			log.Fatal("No Password Given, Exit")
+		}
+
+		// inputTitle not given in cli then using current branch name instead for PR Title
+		if inputTitle == "" {
+			PRTitlePointer = &currentBranch
+		} else {
+			PRTitlePointer = &inputTitle
+		}
+
+		//reviewers
+		reviewers := c.StringSlice("reviewer")
+
+		//username
+		if username == "" {
+			out, err := exec.Command("git", "config", "--global", "user.email").Output()
+			logMessage := "No username found from [git config --global user.email] ;"
+			if err != nil {
+				log.Fatal(logMessage, err)
+			}
+			username = strings.Split(string(out), "@")[0]
+		}
+		if !strings.Contains(username, ".") {
+			log.Fatal("username has no dot symbol inside, ", username)
+		}
+
+		//project and repo
+		out, err := exec.Command("git", "config", "--get", "remote.origin.url").Output()
+		logMessage := "remoteUrl is not found from [git config --get remote.origin.url] ;"
+		if err != nil {
+			log.Fatal(logMessage, err)
+		}
+		remoteUrl := strings.TrimSuffix(string(out), "\n")
+		if remoteUrl == "" {
+			log.Fatal(logMessage, remoteUrl)
+		}
+		urlSplits := strings.Split(remoteUrl, "/")
+		project := urlSplits[len(urlSplits)-2]
+		repo := strings.TrimSuffix(urlSplits[len(urlSplits)-1], ".git")
+
+		//currentBranch
+		out, err = exec.Command("git", "rev-parse", "--abbrev-ref", "HEAD").Output()
+		logMessage = "currentBranch name is not found from [git rev-parse --abbrev-ref HEAD] ;"
+		if err != nil {
+			log.Fatal(logMessage, err)
+		}
+		currentBranch = strings.TrimSuffix(string(out), "\n")
+		if currentBranch == "" {
+			log.Fatal(logMessage, currentBranch)
+		}
+
+		url := serverUrl + "/rest/api/1.0/projects/" + project + "/repos/" + repo + "/pull-requests"
+
+		type user struct {
+			Name string `json:"name"`
+		}
+
+		type reviewerObject struct {
+			User user `json:"user"`
+		}
+
+		var reviewersArray []reviewerObject
+		for _, reviewer := range reviewers {
+			var r reviewerObject
+			r.User.Name = reviewer
+			reviewersArray = append(reviewersArray, r)
+		}
+
+		values := map[string]interface{}{
+			"title":       *PRTitlePointer,
+			"description": description,
+			"fromRef":     map[string]string{"id": "refs/heads/" + currentBranch},
+			"toRef":       map[string]string{"id": "refs/heads/" + destination},
+			"reviewers":   reviewersArray,
+		}
+		jsonValue, _ := json.Marshal(values)
+
+		if c.Bool("debug") {
+			fmt.Println("Post Json - ", string(jsonValue))
+			fmt.Println("username - ", username)
+			fmt.Println("password - ", password)
+			fmt.Println("destBranch - ", destination)
+			fmt.Println("title - ", *PRTitlePointer)
+			fmt.Println("description - ", description)
+			fmt.Println("reviewers - ", reviewers)
+			fmt.Println("currentBranch - ", currentBranch)
+			fmt.Println("repo - ", repo)
+			fmt.Println("project - ", project)
+			fmt.Println("url - ", url)
+		}
+
+		//post Bitbucket API
+		req, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(jsonValue))
+		if err != nil {
+			panic(err)
+		}
+		req.SetBasicAuth(username, password)
+		req.Header.Set("Content-Type", "application/json")
+
+		client := http.DefaultClient
+		resp, err := client.Do(req)
+		if err != nil {
+			panic(err)
+		}
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			panic(err)
+		}
+		defer resp.Body.Close()
+
+		//parse result
+		fmt.Println("---------------------Return -----------------------")
+		if resp.StatusCode >= 200 && resp.StatusCode <= 299 {
+			var result map[string]interface{}
+			err := json.Unmarshal(body, &result)
+			if err != nil {
+				panic(err)
+			}
+			link := result["links"].(map[string]interface{})["self"].([]interface{})[0].(map[string]interface{})["href"]
+			fmt.Println("Succeed")
+			fmt.Println("PR link -> ", link)
+			fmt.Println("Title -> ", *PRTitlePointer)
+		} else {
+			fmt.Println("Argh! Broken")
+			fmt.Printf("%v", string(body))
+		}
+		return nil
+	}
+
+	app.Before = altsrc.InitInputSourceWithContext(flags, altsrc.NewYamlSourceFromFlagFunc("load"))
+	app.Flags = flags
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
